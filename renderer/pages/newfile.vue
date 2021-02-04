df3132d598d28ce16383d30a4eb0d76a5b347b38
SonnyJimm <saraasoulmate@gmail.com>
2021-02-05 01:22:02 +0800

    first commit

---
+++ renderer/pages/newfile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class = "random">
+    <h1>{{ello}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    asyncData ({ req }) {
+    return {
+      ello: "ello"
+    }
+}
+}
+</script>
